8f74ca3b32920c22e016ec223a4e95d9ccda9a21
yellco <dazheltyy@gmail.com>
2022-07-26 01:38:35 +0300

    Fix converting

--- office_converter/convert.py
+++
@@ -1,6 +0,0 @@
-from moviepy.editor import VideoFileClip
-
-
-def convert(video_file):
-    videoClip = VideoFileClip("my-life.mp4")
-    videoClip.write_gif("my-life.gif")
--- office_converter/main.py
+++ office_converter/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, File, UploadFile
-from moviepy.editor import *
+from fastapi import FastAPI, Query
+from moviepy.editor import VideoFileClip
 import os
 
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tmp")
@@ -7,10 +7,28 @@ path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tmp")
 app = FastAPI()
 
 
-@app.post("/convert/", description="Конвертация видео в gif", tags=["Конвертация"])
-async def convert_gif(file: UploadFile):
-    file_bytes = await file.read()
-    f = open(path, 'wb')
-    f.write(file_bytes)
-    f.close()
-    return {"filename": file.filename}
+def output_path(input_path):
+    input_lambda = lambda splitter: input_path.split(splitter)[-1].split(".")[0]
+    if "\\" in input_path:
+        file_input = input_lambda("\\")
+    elif "/" in input_path:
+        file_input = input_lambda("/")
+    else:
+        file_input = "result"
+
+    return os.path.join(os.path.dirname(input_path), f"{file_input}.gif")
+
+
+@app.get("/convert/", description="Конвертация видео в gif", tags=["Конвертация"])
+async def convert_gif(
+        input_path_to_video: str = Query(..., description="Путь для исходного видео файла"),
+        output_gif_path: str = Query(None, description="Путь для сохранения gif файла")
+):
+    if not output_gif_path:
+        output_gif_path = output_path(input_path_to_video)
+    try:
+        video_clip = VideoFileClip(input_path_to_video)
+    except OSError as e:
+        return {"result": "fail", "detail": e}
+    video_clip.write_gif(output_gif_path)
+    return {"result": "success"}
