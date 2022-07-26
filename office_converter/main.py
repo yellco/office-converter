50dd3bd5229bee305377ce08be1a13a04bc7fabb
yellco <dazheltyy@gmail.com>
2022-07-26 23:46:57 +0300

    feat: сделал удобный функционал для конвертации

--- office_converter/main.py
+++ office_converter/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, UploadFile, BackgroundTasks
+from fastapi.responses import FileResponse
 from moviepy.editor import VideoFileClip
 import os
+import aiofiles
+
 
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tmp")
 
 app = FastAPI()
+
+base_path = "./office_converter/tmp"
+
+if not os.path.exists(base_path):
+    os.mkdir(base_path)
 
 
 def output_path(input_path):
@@ -19,16 +27,27 @@ def output_path(input_path):
     return os.path.join(os.path.dirname(input_path), f"{file_input}.gif")
 
 
-@app.get("/convert/", description="Конвертация видео в gif", tags=["Конвертация"])
+def delete_files(*files):
+    for file_path in files:
+        os.remove(file_path)
+
+
+@app.post("/convert", description="Конвертация видео в gif", tags=["Конвертация"])
 async def convert_gif(
-        input_path_to_video: str = Query(..., description="Путь для исходного видео файла"),
-        output_gif_path: str = Query(None, description="Путь для сохранения gif файла")
+    video_file: UploadFile, background_tasks: BackgroundTasks
 ):
-    if not output_gif_path:
-        output_gif_path = output_path(input_path_to_video)
+    path_to_filename = f"{base_path}/{video_file.filename}"
+    result_filename = f"{video_file.filename.split('.')[0]}.gif"
+    result_path = f"{base_path}/{result_filename}"
+    async with aiofiles.open(path_to_filename, 'wb') as out_file:
+        content = await video_file.read()
+        await out_file.write(content)
+
     try:
-        video_clip = VideoFileClip(input_path_to_video)
+        video_clip = VideoFileClip(path_to_filename)
     except OSError as e:
         return {"result": "fail", "detail": e}
-    video_clip.write_gif(output_gif_path)
-    return {"result": "success"}
+    video_clip.write_gif(result_path)
+
+    background_tasks.add_task(delete_files, path_to_filename, result_path)
+    return FileResponse(result_path, filename=result_filename)
